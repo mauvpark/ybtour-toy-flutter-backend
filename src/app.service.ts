f857a574d30855b607f73da8bf22d72919817fba
박현민 <mauvpark@ybtour.co.kr>
2024-07-17 11:01:28 +0900

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '노랑풍선 토이 프로젝트: 플러터용 백엔드 애플리케이션';
   }
 }
