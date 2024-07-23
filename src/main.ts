3caa8a36ffac4eb9b6e0664dfe92a14f6cf5967c
박현민 <mauvpark@ybtour.co.kr>
2024-07-23 10:17:23 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,14 @@ import {
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as hbs from 'hbs';
 import { SWAGGER_URL } from '../constant';
+import path from 'path';
 
 function swaggerConfig(app: NestFastifyApplication) {
   // hbs
-  app.setViewEngine({ engine: { handlebars: hbs }, root: './views' });
+  app.setViewEngine({
+    engine: { handlebars: hbs },
+    root: path.join(process.cwd(), 'views'),
+  });
   // bearer auth
   const config = new DocumentBuilder()
     .addBearerAuth(
